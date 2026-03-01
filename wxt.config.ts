d769145acef5ab304786f02167e4fa5920a56ccc
drag <drag0n1zed@outlook.com>
2026-03-01 19:48:48 +0800

    add wasm build hook

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,22 @@
+import { execSync } from "child_process";
 import { defineConfig } from "wxt";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   srcDir: "src",
+
   vite: () => ({
     plugins: [wasm(), topLevelAwait()],
   }),
+
+  hooks: {
+    // Compile wasm
+    "build:before": (wxt) => {
+      execSync("wasm-pack build --target web", {
+        cwd: "./wasm",
+        stdio: "inherit",
+      });
+    },
+  },
 });
